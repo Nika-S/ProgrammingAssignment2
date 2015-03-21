7b13e3a798f12be132ef2ff6e2245d69caf9aa84
Nika S <sinenko@inbox.lv>
2015-03-21 22:55:45 +0200

    Added  first programm

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,25 @@
 ## initialization example:#  n<-makeCacheMatrix( matrix(c(1, 3, 2, 4),nrow=2,ncol=2))
 
 makeCacheMatrix <- function(x = matrix()) {
+inv <- NULL #Inverse of the matrix x is not known until it is set by setsolve method 
+                    # or  calculated by the function cacheSolve()
 
+        # 'set' method below is used to set new data list instead of the one which was set by creating object
+        set <- function(y) {
+                x <<- y       # reset original matrix x by y, which is provided as an argument of the method 'set'
+                inv <<- NULL    # as we reset the data, so we need to set inverse to NULL for it, preventing 
+                                # keeping old inverse for new data!
+        }
+        get <- function() x     ## this "method" returns unchanged x matrix
+        setsolve <- function(solve) inv <<- solve
+
+        getsolve <- function() inv  # this "method" returns inverse matrix cached previously 
+                                    # (or set by setsolve() or NULL if it wasn't set before  
+
+        # return list of "methods" with their values
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
